79a2d22db683ba7289a32d682d07bf5941a42736
EC2 Default User <ec2-user@ip-172-31-46-14.ap-northeast-1.compute.internal>
2024-08-28 11:19:41 +0000

    tweak btn

--- app/controllers/member/facilities_controller.rb
+++ app/controllers/member/facilities_controller.rb
@@ -63,29 +63,35 @@ end
     end
   end
     
-    def update
-      @facility = Facility.find(params[:id])
-      @facility.tag_list = Tag.where(id: params[:facility][:tag_ids]).pluck(:name).join(',')
-      if @facility.update(facility_params)
-        redirect_to member_facility_path(@facility), notice: '施設が正しく更新されました。'
-      else
-        render :edit
-      end
+  def update
+    @facility = Facility.find(params[:id])
+    @facility.tag_list = Tag.where(id: params[:facility][:tag_ids]).pluck(:name).join(',')
+    if @facility.update(facility_params)
+      redirect_to member_facility_path(@facility), notice: '施設が正しく更新されました。'
+    else
+      render :edit
     end
-    
-    def edit
-      @facility = Facility.find(params[:id])
-    end
-    
-    def destroy
-      @facility = Facility.destroy
+  end
+
+  def edit
+    @facility = Facility.find(params[:id])
+  end
+  
+  def destroy
+    @facility = Facility.destroy
       redirect_to member_facilities_path, notice: '登録した施設を削除しました。'
+  end
+    
+  def search
+    @facilities = Facility.search(params[:keyword]).page(params[:page]).per(10)
+    
+    # 各施設ごとの評価を計算
+    @facility_ratings = @facilities.each_with_object({}) do |facility, hash|
+      valid_comments = facility.comments.joins(:member).where(members: { is_deleted: false })
+      hash[facility.id] = valid_comments.average(:rate).to_f.round(1) if valid_comments.present?
     end
     
-    def search
-      @facility_ratings = {} unless @facility_ratings
-      @facilities = Facility.search(params[:keyword]).page(params[:page]).per(10)
-      render 'index'
+    render 'index'
     end
     
     private
